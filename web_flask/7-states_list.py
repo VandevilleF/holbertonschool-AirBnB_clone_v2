ad1369265276ec3d8b320a80a80909c9582d25d6
VandevilleF <flo.vandeville@gmail.com>
2024-04-08 19:05:13 +0200

    change name of function in app.teardown and add option

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,14 +9,16 @@ app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_session():
+def close(exception):
+    """Close storage"""
     storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
 def states():
+    """List all states"""
     return render_template("7-states_list.html", states=storage.all(State))
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
