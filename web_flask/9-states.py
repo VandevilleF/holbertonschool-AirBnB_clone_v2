9503efb05ab1c8079fd9c9f44d3f24d3cc7879c4
VandevilleF <flo.vandeville@gmail.com>
2024-04-09 14:17:49 +0200

    Add files for task 10, local test OK

---
+++ web_flask/9-states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close(exception):
+    """Close storage"""
+    storage.close()
+
+
+@app.route("/states", strict_slashes=False)
+def states():
+    """List all states"""
+    return render_template("9-states.html", states=storage.all(State))
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def cities_state_id(id):
+    """List all states or state found with id"""
+    states = storage.all(State).values()
+    for obj in states:
+        if id == obj.id:
+            state = obj
+    return render_template('9-states.html', state=state)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
