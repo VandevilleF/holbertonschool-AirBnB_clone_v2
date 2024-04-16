6199ee3f4ea4980c021ee1f963f9326d82f52899
Florent1711 <graphit01@hotmail.com>
2024-04-16 12:54:40 +0200

    Ajout de user.py, city.py

--- models/place.py
+++ models/place.py
@@ -1,18 +1,40 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base, ForeignKey
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    __tablename__ = 'places'  # Table name
+
+    # City ID (foreign key to cities.id)
+    city_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+
+    # User ID (foreign key to users.id)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+
+    # Name of the place
+    name = Column(String(60), ForeignKey('users.id'), nullable=False)
+
+    # Description of the place (optional)
+    description = Column(String(1024))
+
+    # Numbers of rooms
+    number_rooms = Column(Integer, nullable=False, default=0)
+
+    # Number of bathroom
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+
+    # Maximum number of guests
+    max_guest = Column(Integer, nullable=False, default=0)
+
+    # Price per night
+    price_by_night = Column(Integer, nullable=False, default=0)
+
+    # Latitude coordinate (optional)
+    latitude = Column(Float)
+
+    # Longitude coordinate (optionnal)
+    longitude = Column(Float)
+
     amenity_ids = []
